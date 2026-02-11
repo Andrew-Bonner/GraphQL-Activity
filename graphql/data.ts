034df5fc93b8458fabc63b185787df45810e5a73
Andrew-Bonner <andrew.bonner@bsu.edu>
2026-02-11 00:16:19 -0500

    initial commit

---
+++ graphql/data.ts
@@ -0,0 +1,26 @@
+export const db = [
+    {
+        zip: "44146",
+        weather: "sunny",
+        tempC: "25C",
+        tempF: "70F",
+        precipitation: null,
+        friends: ["44300", "47304"]
+    },
+    {
+        zip: "47304",
+        weather: "sunny",
+        tempC: "30C",
+        tempF: "86F",
+        precipiatation: "10%",
+        friends: ["44300", "44146"] 
+    },
+    {
+        zip: "44300",
+        weather: "sunny",
+        tempC: "20-C",
+        tempF: "68F",
+        precipitation: "20%",
+        friends: ["44146", "47304"]
+    }
+];
